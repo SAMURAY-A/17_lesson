e5bae9423c650347ff48d20f0d5bfc4bae7e72ad
SAMURAY-A <abdullohaziz559@gmail.com>
2024-12-16 10:33:23 +0500

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+// src/components/Loader.jsx
+
+export default function Loader() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <span className="loading loading-dots loading-lg"></span>
+        </div>
+    );
+}
